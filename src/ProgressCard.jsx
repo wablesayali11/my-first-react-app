5876a13f92946f4b8db357c029a97c6077734a6d
Sayali Wable <wablesayali14@gmail.com>
2026-08-08 13:26:42 +0530

    Added interactive progress with useState

--- src/ProgressCard.jsx
+++ src/ProgressCard.jsx
@@ -1,8 +1,30 @@
-function ProgressCard({ title, completed, total }) {
+import { useState } from "react";
+
+function ProgressCard(props) {
+  const [completed, setCompleted] = useState(props.completed);
+
+  function increaseProgress() {
+    setCompleted(completed + 1);
+  }
+  function decreaseProgress() {
+    setCompleted(completed - 1);
+  }
+
+
   return (
     <div>
-      <h2>{title}</h2>
-      <p>{completed} / {total}</p>
+      <h2>{props.title}</h2>
+
+      <p>
+        {completed} / {props.total}
+      </p>
+
+      <button onClick={increaseProgress}>
+        +1 Problem
+      </button>
+      <button onClick={decreaseProgress}>
+        -1 Problem
+      </button>
     </div>
   );
 }
