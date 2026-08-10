91ac4391403077817fbbf9cf14605765eff78a16
Sayali Wable <wablesayali14@gmail.com>
2026-08-10 13:34:17 +0530

    Added dynamic skill cards with map

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
-import Navbar from "./Navbar";
-import ProgressCard from "./ProgressCard";
+import Navbar from "./Navbar.jsx";
+import ProgressCard from "./ProgressCard.jsx";
 
 function App() {
+  const skills = [
+    {
+      title: "DSA",
+      completed: 42,
+      total: 100
+    },
+    {
+      title: "React",
+      completed: 10,
+      total: 50
+    },
+    {
+      title: "Java",
+      completed: 20,
+      total: 50
+    },
+    {
+      title: "Python",
+      completed: 15,
+      total: 50
+    },
+    {
+      title:"JavaScript",
+      completed: 25,
+      total: 50
+    },
+    {
+      title:"CSS",
+      completed: 30,
+      total: 40
+    }
+
+  ];
+
   return (
     <div>
       <Navbar />
@@ -9,35 +43,14 @@ function App() {
       <h1>Welcome to CodeTrack</h1>
       <p>My developer progress tracker</p>
 
-      <ProgressCard
-  title="DSA"
-  completed={42}
-  total={100}
-/>
-
-<ProgressCard
-  title="React"
-  completed={10}
-  total={50}
-/>
-
-<ProgressCard
-  title="Java"
-  completed={20}
-  total={50}
-/>
-
-<ProgressCard
-  title="Python"
-  completed={15}
-  total={50}
-/>
-
-<ProgressCard
-  title="HTML"
-  completed={30}
-  total={40}
-/>
+     {skills.map((skill) => (
+  <ProgressCard
+    key={skill.title}
+    title={skill.title}
+    completed={skill.completed}
+    total={skill.total}
+  />
+))}
     </div>
   );
 }
