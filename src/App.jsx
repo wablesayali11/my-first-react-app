346c760c09a962e2a95efb1e5d7b6cb1e37532ef
Sayali Wable <wablesayali14@gmail.com>
2026-08-14 00:03:10 +0530

    Added local storage for goals

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GoalForm from "./GoalForm.jsx";
 import Navbar from "./Navbar.jsx";
 import ProgressCard from "./ProgressCard.jsx";
@@ -37,44 +37,57 @@ function App() {
     }
   ];
 
-  // Store goals
-  const [goals, setGoals] = useState([]);
+  // Load saved goals from localStorage
+  const [goals, setGoals] = useState(() => {
+    const savedGoals = localStorage.getItem("goals");
+
+    return savedGoals ? JSON.parse(savedGoals) : [];
+  });
 
   // Add a new goal
   function addGoal(newGoal) {
     setGoals([...goals, newGoal]);
   }
+
+  // Delete a goal
   function deleteGoal(indexToDelete) {
-  setGoals(goals.filter((_, index) => index !== indexToDelete));
-}
+    setGoals(
+      goals.filter((_, index) => index !== indexToDelete)
+    );
+  }
+
+  // Save goals whenever goals change
+  useEffect(() => {
+    localStorage.setItem("goals", JSON.stringify(goals));
+  }, [goals]);
 
   return (
     <div>
       <Navbar />
 
       <h1>Welcome to CodeTrack</h1>
+
       <p>My developer progress tracker</p>
 
       <h2>Today's Goals</h2>
 
       {/* Goal Form */}
       <GoalForm onAddGoal={addGoal} />
-      
 
       {/* Display Goals */}
       <h3>Your Goals</h3>
 
-     <ul>
-     {goals.map((goal, index) => (
-      <li key={index}>
-     {goal}
+      <ul>
+        {goals.map((goal, index) => (
+          <li key={index}>
+            {goal}
 
-      <button onClick={() => deleteGoal(index)}>
-        Delete
-      </button>
-      </li>
-    ))}
-     </ul>
+            <button onClick={() => deleteGoal(index)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
 
       {/* Progress Cards */}
       {skills.map((skill) => (
